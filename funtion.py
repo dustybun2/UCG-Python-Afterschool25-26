d0ce19e1b4d624895865e184432199322c0899fe
dustybun2 <Dawashington07@gmail.com>
2025-11-20 16:30:03 -0800

    Add files via upload

---
+++ funtion.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+print(f"The numbers {num1} and {num2} added together = {add(num1, num2)}")
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a*b
+
+print(f"The numbers {num1} and {num2} subtracted together = {sub(num1, num2)}")
+print(f"The numbers {num1} and {num2} multiplied together = {mul(num1,num2)}")
