cc8ba7d5a3c6e0da82ad8d23eb11865ed4f6845d
dustybun2 <Dawashington07@gmail.com>
2025-12-02 16:42:24 -0800

    Add files via upload

---
+++ StringReverse.py
@@ -0,0 +1,42 @@
+word = str(input("please enter a word:"))
+
+
+
+
+
+
+re_word="" 
+
+
+
+
+
+
+
+i =len(word) -1
+
+
+
+
+
+
+while i >= 0: 
+  
+
+
+
+
+
+    re_word+= word[i]
+
+
+
+
+
+
+    i -= 1 
+
+
+
+
+print(re_word)
